3d613b931ee7857c90ea1ace7654bf3fb2be321c
Vincent Fumo <neodem@gmail.com>
2012-09-30 13:16:02 -0400

    adds collections tool

---
+++ src/main/java/com/neodem/common/utility/Collections.java
@@ -0,0 +1,15 @@
+package com.neodem.common.utility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Collections {
+
+	public static <K, V> Map<K, V> copyMap(Map<K,V> from){
+		Map<K, V> copy = new HashMap<K, V>(from.size());
+		for(K key : copy.keySet()) {
+			copy.put(key, from.get(key));
+		}
+		return copy;
+	}
+}
