7fc1c432dc1046cc45d8cdb515d7e968232f76df
Vincent Fumo <neodem@gmail.com>
2012-11-24 15:02:44 -0500

    adds new Chars class

--- src/main/java/com/neodem/common/utility/Collections.java
+++ src/main/java/com/neodem/common/utility/Collections.java
@@ -7,7 +7,7 @@ public class Collections {
 
 	public static <K, V> Map<K, V> copyMap(Map<K,V> from){
 		Map<K, V> copy = new HashMap<K, V>(from.size());
-		for(K key : copy.keySet()) {
+		for(K key : from.keySet()) {
 			copy.put(key, from.get(key));
 		}
 		return copy;
---
+++ src/main/java/com/neodem/common/utility/text/Chars.java
@@ -0,0 +1,5 @@
+package com.neodem.common.utility.text;
+
+public class Chars {
+	public static final char NEWLINE = '\n';
+}
