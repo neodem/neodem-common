36fa40db1d19903707f388753a6c6de9fe813294
Vincent Fumo <vincent_fumo@cable.comcast.com>
2014-03-24 10:37:06 -0400

    adds comments

--- src/main/java/com/neodem/common/utility/collections/Lists.java
+++ src/main/java/com/neodem/common/utility/collections/Lists.java
@@ -7,7 +7,19 @@ import java.util.List;
  */
 public class Lists {
 
+    /**
+     * return a reordered List starting with the given element or return an
+     * empty list if the element isn't part of the original list or
+     * the original list is null
+     *
+     * @param theList
+     * @param startingWith
+     * @param <T>
+     * @return
+     */
     public static <T> List<T> reorder(List<T> theList, T startingWith) {
+        if(theList == null) return com.google.common.collect.Lists.newArrayList();
+
         int startIndex = theList.lastIndexOf(startingWith);
         if (startIndex == -1) return com.google.common.collect.Lists.newArrayList();
 
